1c0bec7c05208546ce335e5cbdabf58c87d38fac
alex hawley <asimo124@yahoo.com>
2016-06-07 09:34:51 -0500

    2016-06-07 search_rev changes

--- git/search_rev.sh
+++ git/search_rev.sh
@@ -1,13 +1,30 @@
+#!/bin/ash
 
-# git log --pretty=tformat:'%H' > git_logs.txt
+git log > git_logs_full.txt
+git log --pretty=tformat:'%H' > git_logs.txt
 
 while read p; do
 
     count=$(echo $p | xargs git show --pretty="format:" --name-only | grep $1 | wc -l)
 
     if [ "$count" -gt "0" ]; then
-        echo $p
+        #echo $p
+
+        grep -A 3 `echo $p` git_logs_full.txt
+
+
+
         echo $p | xargs git show --pretty="format:" --name-only | grep $1
+
+        echo " "
+
+
+        echo "git show $p:$1 | gitcat"
+        echo "git diff $p:$1 XXXXXXX:$1"
+        echo "git diff XXXXXXX:$1 $p:$1"
+
+        echo " "
+        echo " "
         echo " "
     fi
 
--- search_rev.sh
+++ search_rev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 
 git log > git_logs_full.txt
 git log --pretty=tformat:'%H' > git_logs.txt
@@ -12,7 +12,18 @@ while read p; do
 
         grep -A 3 `echo $p` git_logs_full.txt
 
+
+
         echo $p | xargs git show --pretty="format:" --name-only | grep $1
+
+        echo " "
+
+
+        echo "git show $p:$1 | gitcat"
+        echo "git diff $p:$1 XXXXXXX:$1"
+        echo "git diff XXXXXXX:$1 $p:$1"
+
+        echo " "
         echo " "
         echo " "
     fi
