804cb7e878614839a32504dfaf08062ab4871ef4
Alex Hawley <ahawley@Alexs-IMac.local>
2016-12-09 14:22:41 -0600

    Git file changes

--- git_commit_files.sh
+++ git_commit_files.sh
@@ -11,10 +11,10 @@ while read p; do
 done < git_commits.txt
 
 grep -o '.\{40\}$' run_commit_show2.txt > run_commit_show3.sh
-sed -i "" 's/\([a-zA-Z0-9]\{40\}\)/git show --pretty=\"format\: \" --name-only \1/g' run_commit_show3.sh
+sed -i "" 's/\([a-zA-Z0-9]\{40\}\)/echo \"_________________________________\";echo \"\1\"\; echo \"_________________________________\";git show --pretty=\"format\: \" --name-only \1;echo " ";/g' run_commit_show3.sh
 
 chmod 755 run_commit_show3.sh
+./run_commit_show3.sh
 ./run_commit_show3.sh > commit_file_list2.txt
 
-cat commit_file_list2.txt | sort | uniq > commit_file_list.txt
-cat commit_file_list.txt
+#cat commit_file_list2.txt | sort | uniq > commit_file_list.txt
