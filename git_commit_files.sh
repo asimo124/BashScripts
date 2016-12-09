2d137dd11805b92c68a47b9c3dad803c9aec2a42
Alex Hawley <ahawley@Alexs-IMac.local>
2016-12-09 14:08:55 -0600

    Git Script to find all files

---
+++ git_commit_files.sh
@@ -0,0 +1,20 @@
+
+
+
+grep -h --color=always 'commit ' git_logs.txt | sed 's/commit //g' > git_commits.txt  
+
+echo "" > commit_file_list2.txt
+echo "" > run_commit_show2.txt
+while read p; do
+    #echo "git show --pretty=\"format: --name-only \" $p" >> run_commit_show.sh
+    echo "$p" >> run_commit_show2.txt
+done < git_commits.txt
+
+grep -o '.\{40\}$' run_commit_show2.txt > run_commit_show3.sh
+sed -i "" 's/\([a-zA-Z0-9]\{40\}\)/git show --pretty=\"format\: \" --name-only \1/g' run_commit_show3.sh
+
+chmod 755 run_commit_show3.sh
+./run_commit_show3.sh > commit_file_list2.txt
+
+cat commit_file_list2.txt | sort | uniq > commit_file_list.txt
+cat commit_file_list.txt
